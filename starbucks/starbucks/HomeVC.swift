f9ef993a169a6312590359181a89a21e3f2296bc
MelonMania <dnwlsskfn@gmail.com>
2022-04-20 23:40:38 +0900

    Home화면 UI 구성

---
+++ starbucks/starbucks/HomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  starbucks
+//
+//  Created by RooZin on 2022/04/20.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
